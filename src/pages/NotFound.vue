7831eb944fc9ed4ee06f90f35c0817b1e6b1da1e
ziad mohamed <ziadmohad45@gmail.com>
2023-09-22 10:10:32 +0300

    set the base of not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<template>    
+    <section>
+        <div class="container my-5 p-3">
+          <h1> Not found bage</h1>
+          <p> maybe there are somthing in this bage , go to <router-link to="/home" > home bage</router-link> </p>
+        </div>
+    </section>
+</template>
+
