b0dcd675181b84fcb4bec77c0cf1685231c1a275
ziad mohamed <ziadmohad45@gmail.com>
2023-09-22 10:14:02 +0300

    modify the found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,9 +1,11 @@
-<template>    
-    <section>
-        <div class="container my-5 p-3">
-          <h1> Not found bage</h1>
-          <p> maybe there are somthing in this bage , go to <router-link to="/home" > home bage</router-link> </p>
-        </div>
-    </section>
+<template>
+  <section>
+    <div class="container my-5 p-3">
+      <h1>Not found bage</h1>
+      <p>
+        Maybe there are somthing in this bage , go to
+        <router-link to="/home"> home bage</router-link>
+      </p>
+    </div>
+  </section>
 </template>
-
