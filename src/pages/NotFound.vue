67561079331eb4fbb92d49e83c4fa426c6382d20
ziad mohamed <77482848+ziaaddd@users.noreply.github.com>
2023-09-22 10:28:54 +0300

    Update NotFound.vue
    just doing small update

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -3,7 +3,7 @@
     <div class="container my-5 p-3">
       <h1>Not found bage</h1>
       <p>
-        Maybe there are somthing in this bage , go to
+        Maybe there are somthing in this bage, Go to
         <router-link to="/home"> home bage</router-link>
       </p>
     </div>
